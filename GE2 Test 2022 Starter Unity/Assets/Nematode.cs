e21425759289610978e4aea993e3d251e11f51f2
SKIDILYBOP <conorhartnett6898@hotmail.com>
2022-04-26 11:57:00 +0100

    C19729601 Submission

--- GE2 Test 2022 Starter Unity/Assets/Nematode.cs
+++ GE2 Test 2022 Starter Unity/Assets/Nematode.cs
@@ -8,16 +8,32 @@ public class Nematode : MonoBehaviour
 
     public Material material;
 
+    public int SphereRange;
+
+    public ArrayList MultiSphere = new ArrayList();
+
+    [SerializeField] Vector3 spacing = new Vector3(1, 0, 0);
+
     void Awake()
     {
-        // Put your code here!
+        SphereRange = Random.Range(5, 10);
+        GameObject SphereLead = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+        SphereLead.transform.position = gameObject.transform.position;
+
+        for (int i = 0; i < SphereRange; i++)
+        {
+            GameObject Spheres = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+            MultiSphere.Add(Spheres);
+            Spheres.transform.position = (SphereLead.transform.position += Vector3.right * 1f);
+            
+        }
     }
 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        Awake();
     }
 
     // Update is called once per frame
